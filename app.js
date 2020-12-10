de215bf31d76107a9602a8b64298ae7df5a69646
Luis Rujana <lmrujanass@gmail.com>
2020-12-09 23:49:33 -0500

    Created the createNewTeam() function

--- app.js
+++ app.js
@@ -14,9 +14,122 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+// const employees = [];
+
+const createNewTeam = async (employees = []) => {
+      const newEmployeeInputs = await inquirer.prompt([ {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of a new team member:'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter member's ID:"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter member's email:"
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: "Enter member's role:",
+        choices: ['Manager', 'Engineer', 'Intern']
+    }]);
+
+
+
+    switch(newEmployeeInputs.role){
+        case 'Manager':
+            const managerSpec = await inquirer.prompt([
+                {
+                    type: 'number',
+                    name: 'officeNumber',
+                    message: "Enter Manager's office number:"
+                },
+                {
+                    type: 'confirm',
+                    name: 'again',
+                    message: 'Do you want to add another member?'
+                }
+            ]);
+            newEmployeeInputs.officeNumber = managerSpec.officeNumber;
+            newEmployeeInputs.again = managerSpec.again;
+        break;
+
+        case 'Engineer':
+            const engineerSpec = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: "Enter Engineer's GitHub user:"
+                },
+                {
+                    type: 'confirm',
+                    name: 'again',
+                    message: 'Do you want to add another member?'
+                }
+
+            ]);
+            newEmployeeInputs.github = engineerSpec.github;
+            newEmployeeInputs.again = engineerSpec.again;
+        break;
+
+        case 'Intern':
+            const internSpec = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: "Enter Intern's school name:"
+                },
+                {
+                    type: 'confirm',
+                    name: 'again',
+                    message: 'Do you want to add another member?'
+                }
+            ]);
+            newEmployeeInputs.school = internSpec.school;
+            newEmployeeInputs.again = internSpec.again;
+        break;
+    }
+
+    let newEmployee = {};
+    let updatedEmployees = [];
+
+    switch(newEmployeeInputs.role){
+        case 'Manager':
+            newEmployee = new Manager(newEmployeeInputs.name, newEmployeeInputs.id, newEmployeeInputs.email, newEmployeeInputs.officeNumber);
+            updatedEmployees = [...employees, newEmployee];
+            return newEmployeeInputs.again ? createNewTeam(updatedEmployees) : updatedEmployees;
+        break;
+        case 'Engineer':
+            newEmployee = new Engineer(newEmployeeInputs.name, newEmployeeInputs.id, newEmployeeInputs.email, newEmployeeInputs.github);
+            updatedEmployees = [...employees, newEmployee];
+            return newEmployeeInputs.again ? createNewTeam(updatedEmployees) : updatedEmployees;
+        break;
+        case 'Intern':
+            newEmployee = new Intern(newEmployeeInputs.name, newEmployeeInputs.id, newEmployeeInputs.email, newEmployeeInputs.school);
+            updatedEmployees = [...employees, newEmployee];
+            return newEmployeeInputs.again ? createNewTeam(updatedEmployees) : updatedEmployees;
+    }
+}
+
+
+const init = async () =>{
+    const team = await createNewTeam();
+    console.log(team);
+
+};
+
+init();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
